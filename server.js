99e6cff6f579e5ede67c182dbab5547ec59279db
David Clark <dlc.clark@gmail.com>
2018-08-16 19:15:15 -0700

    css error

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
-app.use(express.static('css'))
 app.listen(process.env.PORT || 8080)
 
 module.exports = app
