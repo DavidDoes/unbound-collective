bf6e20b60b873d2e314b9894a4eef61a25029b97
David Clark <dlc.clark@gmail.com>
2018-08-21 20:42:00 -0700

    fix endpoints

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
 'use strict'
 
-exports.SUBMISSIONS_DB_URL = process.env.SUBMISSIONS_DB_URL || 8080 || 'mongodb://localhost/challenge-photos';
-
-exports.CHALLENGES_DB_URL = process.env.CHALLENGES_DB_URL ||
-'mongodb://localhost/challenge-photos';
-
 exports.USERS_DB_URL = process.env.USERS_DB_URL ||
-'mongodb://localhost/challenge-photos';
+'mongodb://localhost/challenge-photos/users';
 
 exports.PORT = process.env.PORT || 8080
