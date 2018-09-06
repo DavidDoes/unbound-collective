bd1afa1a386aff36b5ba5705ec135bb54eeef6af
David Clark <dlc.clark@gmail.com>
2018-09-05 17:59:52 -0700

    .

--- public/app.js
+++ public/app.js
@@ -1,52 +1,47 @@
-'use strict';
+// 'use strict';
 
-function getChallenges(callback){
-  setTimeout(function(){
-    callback(MOCK_CHALLENGES)
-  }, 100
-  )
-}
-//display title and desc:
-function displayChallenges(data){ 
-  // console.log(data)
-  for (index in data.challenges){ 
-    $('#challenges').append(
-      '<div class="one-third"><img class="thumbnail" src="' + data.challenges[index].thumbnail + '"><h2>' + data.challenges[index].title + '</h2><p>' + data.challenges[index].description + '</p><p>' + data.challenges[index].entries + ' entries</p><p>Created by ' + data.challenges[index].creator + '</p></div>'
-    )
-  }
-}
-//run fns above
-function getAndDisplayChallenges(){
-  getChallenges(displayChallenges)
-}
+// // function getChallenges(){
 
-//SUBMISSIONS ON DASHBOARD
-function getUserSubmissions(callback){
-  setTimeout(function(){
-    callback(MOCK_PHOTOS)
-  }, 100
-  )
-}
+// // };
 
-function displayUserSubmissions(data){
-  // console.log(data)
-  let currentUser = "ProUser" // username of current logged-in user
+// //display title and desc:
+// function displayChallenges(data){ 
+//   // console.log(data)
+//   for (index in data.challenges){ 
+//     $('#challenges').append(
+//       '<div class="one-third"><img class="thumbnail" src="' + data.challenges[index].thumbnail + '"><h2>' + data.challenges[index].title + '</h2><p>' + data.challenges[index].description + '</p><p>' + data.challenges[index].entries + ' entries</p><p>Created by ' + data.challenges[index].creator + '</p></div>'
+//     )
+//   }
+// }
+// //run fns above
+// function getAndDisplayChallenges(){
+//   getChallenges(displayChallenges)
+// }
 
-  for (index in data.submissions){ 
-    if (data.submissions[index].creator === currentUser){
-      $('#userSubmissions').append(
-        '<div class="one-third"><p>Submitted to Challenge <b>' + data.submissions[index].challenge + '</b></p><p>' + 'Submitted ' + data.submissions[index].dateCreated + '</p></div>'
-      )
-    }
-  }
-}
+// //SUBMISSIONS ON DASHBOARD
+// // function getUserSubmissions(callback){
 
-function getAndDisplayUserSubmissions(){
-  getUserSubmissions(displayUserSubmissions)
-}
+// // };
+
+// function displayUserSubmissions(data){
+//   // console.log(data)
+//   let currentUser = "ProUser" // username of current logged-in user
+
+//   for (index in data.submissions){ 
+//     if (data.submissions[index].creator === currentUser){
+//       $('#userSubmissions').append(
+//         '<div class="one-third"><p>Submitted to Challenge <b>' + data.submissions[index].challenge + '</b></p><p>' + 'Submitted ' + data.submissions[index].dateCreated + '</p></div>'
+//       )
+//     }
+//   }
+// }
+
+// function getAndDisplayUserSubmissions(){
+//   getUserSubmissions(displayUserSubmissions)
+// }
 
 
-$(function(){
-  getAndDisplayChallenges()
-  getAndDisplayUserSubmissions()
-})
+// $(function(){
+//   getAndDisplayChallenges()
+//   getAndDisplayUserSubmissions()
+// })
